5108208fc18193d1c54ec98f6275af9d3a8cd232
Diogo Coelho <diogooliveiracoelho@gmail.com>
2026-08-21 12:09:34 -0300

    Inclusão de pacote typeorm nos projeto de backend

--- balloon-core-service/src/app.module.ts
+++ balloon-core-service/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_DATABASE_HOST,
+      port: Number(process.env.PG_DATABASE_PORT),
+      database: process.env.PG_DATABASE_NAME,
+      username: process.env.PG_DATABASE_USERNAME,
+      password: process.env.PG_DATABASE_PASSWORD,
+      autoLoadEntities: true,
     }),
   ],
   controllers: [],
