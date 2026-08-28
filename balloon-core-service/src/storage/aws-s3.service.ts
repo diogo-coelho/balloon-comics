2d433378572ca4e2ace8f73847810ed3855bebfa
Diogo Coelho <diogooliveiracoelho@gmail.com>
2026-08-28 14:02:48 -0300

    Estruturação do docker dos serviços

--- balloon-core-service/src/storage/aws-s3.service.ts
+++ balloon-core-service/src/storage/aws-s3.service.ts
@@ -9,11 +9,7 @@ export class AwsS3Service implements StorageService {
   private readonly storage: S3Client = new S3Client({
     region: process.env.AWS_REGION as string,
     endpoint: process.env.AWS_S3_ENDPOINT || undefined,
-    forcePathStyle: !!process.env.AWS_S3_ENDPOINT,
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
-    },
+    forcePathStyle: Boolean(!!process.env.AWS_S3_ENDPOINT),
   });
   
   async uploadFile(file: Express.Multer.File, object: string) {
